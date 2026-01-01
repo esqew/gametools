e7c3599f96f5d9db176982ea45e517a8a2be3ebf
Sean F Quinn <sean@esqew.com>
2026-01-01 00:56:44 -0500

    Improve test coverage of player module

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,5 +1,5 @@
 import pytest
-from gametools.player import CardPlayer
+from gametools.player import Player, CardPlayer
 
 class TestCardPlayer:
     @pytest.fixture
@@ -8,4 +8,21 @@ class TestCardPlayer:
     
     def test_initialization(self, player: CardPlayer):
         assert player.name == "Poker Player"
-        assert player.hand is not None
+        assert len(player.hand) == 0
+        assert player.bank.balance == 100.0 # pyright: ignore[reportOptionalMemberAccess]
+
+    def test_give_funds(self, player: CardPlayer):
+        recipient = CardPlayer(name="Recipient", starting_balance=50.0)
+        player.give(30.0, to=recipient)
+        
+        assert player.bank.balance == 70.0 # pyright: ignore[reportOptionalMemberAccess]
+        assert recipient.bank.balance == 80.0 # pyright: ignore[reportOptionalMemberAccess]
+
+    def test_give_funds_no_bank(self, player: CardPlayer):
+        recipient = Player(name="Recipient")
+        
+        with pytest.raises(ValueError, match="does not have a bank"):
+            player.give(30.0, to=recipient)
+
+        with pytest.raises(ValueError, match="does not have a bank"):
+            recipient.give(30.0, to=player)
