0e23cbd77035712d3502610dcea7c44f650618fc
Sean F Quinn <sean@esqew.com>
2026-01-01 00:56:37 -0500

    Re-organization of some of the implementation from CardPlayer to Player base class

--- gametools/player.py
+++ gametools/player.py
@@ -4,7 +4,35 @@ from .bank import PlayerBank
 
 class Player:
     name: str
+    bank: Optional[PlayerBank]
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.bank = None
+        return
+    
+    def give(self, amount: float | int, to: 'Player') -> None:
+        """
+        Transfer an amount from this `Player`'s bank to another `Player`'s bank.
+
+        Args:
+            amount (float | int): The amount to transfer.
+            to (Player): The target `Player` to receive the funds.
+
+        Raises:
+            ValueError: If either player does not have a bank.
+        """
+        if self.bank is None:
+            raise ValueError(f"Player {self.name} does not have a bank")
+        if to.bank is None:
+            raise ValueError(f"Player {to.name} does not have a bank")
+        
+        return self.bank.give(amount=amount, to=to.bank)
 
 class CardPlayer(Player):
-    hand: CardCollection
-    bank: Optional[PlayerBank]
+    hand: CardCollection = CardCollection()
+
+    def __init__(self, name: str, starting_balance: float = 100.0) -> None:
+        super().__init__(name=name)
+        self.bank = PlayerBank(starting_balance=starting_balance)
+        return
